5152220f4fa3f37be8dfa9c7fd0d2dbdc71846a1
武田元 <>
2025-06-18 09:32:29 +0900

    SECRET_KEYを.env化

--- config/settings.py
+++ config/settings.py
@@ -36,7 +36,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!(dt@f9o72hqkk#@*k1e)6ynw4u!_d)gk@fjb22d3+lgh3j6ll'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
