58d657d3c5fbdade24112b1d4f29b44c421a14ba
武田元 <>
2025-06-24 11:50:15 +0900

    設定ファイルやビューを修正

--- diary/models.py
+++ diary/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User  # ユーザーモデルのインポート
 
 class Diary(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)  # ← 追加
     title = models.CharField(
         max_length=100, 
         blank=False,
